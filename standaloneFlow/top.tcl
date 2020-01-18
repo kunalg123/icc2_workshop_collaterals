a36f27f559b2c4e5c4d79604e61286825324986a
Kunal Ghosh <kunalpghosh@gmail.com>
2020-01-18 13:45:07 +0530

    Uploading standalone script for PNR flow

---
+++ standaloneFlow/top.tcl
@@ -0,0 +1,170 @@
+source -echo ./icc2_common_setup.tcl
+source -echo ./icc2_dp_setup.tcl
+if {[file exists ${WORK_DIR}/$DESIGN_LIBRARY]} {
+   file delete -force ${WORK_DIR}/${DESIGN_LIBRARY}
+}
+###---NDM Library creation---###
+set create_lib_cmd "create_lib ${WORK_DIR}/$DESIGN_LIBRARY"
+if {[file exists [which $TECH_FILE]]} {
+   lappend create_lib_cmd -tech $TECH_FILE ;# recommended
+} elseif {$TECH_LIB != ""} {
+   lappend create_lib_cmd -use_technology_lib $TECH_LIB ;# optional
+}
+lappend create_lib_cmd -ref_libs $REFERENCE_LIBRARY
+puts "RM-info : $create_lib_cmd"
+eval ${create_lib_cmd}
+
+###---Read Synthesized Verilog---###
+if {$DP_FLOW == "hier" && $BOTTOM_BLOCK_VIEW == "abstract"} {
+   # Read in the DESIGN_NAME outline.  This will create the outline
+   puts "RM-info : Reading verilog outline (${VERILOG_NETLIST_FILES})"
+   read_verilog_outline -design ${DESIGN_NAME}/${INIT_DP_LABEL_NAME} -top ${DESIGN_NAME} ${VERILOG_NETLIST_FILES}
+   } else {
+   # Read in the full DESIGN_NAME.  This will create the DESIGN_NAME view in the database
+   puts "RM-info : Reading full chip verilog (${VERILOG_NETLIST_FILES})"
+   read_verilog -design ${DESIGN_NAME}/${INIT_DP_LABEL_NAME} -top ${DESIGN_NAME} ${VERILOG_NETLIST_FILES}
+}
+
+## Technology setup for routing layer direction, offset, site default, and site symmetry.
+#  If TECH_FILE is specified, they should be properly set.
+#  If TECH_LIB is used and it does not contain such information, then they should be set here as well.
+if {$TECH_FILE != "" || ($TECH_LIB != "" && !$TECH_LIB_INCLUDES_TECH_SETUP_INFO)} {
+   if {[file exists [which $TCL_TECH_SETUP_FILE]]} {
+      puts "RM-info : Sourcing [which $TCL_TECH_SETUP_FILE]"
+      source -echo $TCL_TECH_SETUP_FILE
+   } elseif {$TCL_TECH_SETUP_FILE != ""} {
+      puts "RM-error : TCL_TECH_SETUP_FILE($TCL_TECH_SETUP_FILE) is invalid. Please correct it."
+   }
+}
+
+# Specify a Tcl script to read in your TLU+ files by using the read_parasitic_tech command
+if {[file exists [which $TCL_PARASITIC_SETUP_FILE]]} {
+   puts "RM-info : Sourcing [which $TCL_PARASITIC_SETUP_FILE]"
+   source -echo $TCL_PARASITIC_SETUP_FILE
+} elseif {$TCL_PARASITIC_SETUP_FILE != ""} {
+   puts "RM-error : TCL_PARASITIC_SETUP_FILE($TCL_PARASITIC_SETUP_FILE) is invalid. Please correct it."
+} else {
+   puts "RM-info : No TLU plus files sourced, Parastic library containing TLU+ must be included in library reference list"
+}
+
+###---Routing settings---###
+## Set max routing layer
+if {$MAX_ROUTING_LAYER != ""} {set_ignored_layers -max_routing_layer $MAX_ROUTING_LAYER}
+## Set min routing layer
+if {$MIN_ROUTING_LAYER != ""} {set_ignored_layers -min_routing_layer $MIN_ROUTING_LAYER}
+
+####################################
+# Check Design: Pre-Floorplanning
+####################################
+if {$CHECK_DESIGN} {
+   redirect -file ${REPORTS_DIR_INIT_DP}/check_design.pre_floorplan     {check_design -ems_database check_design.pre_floorplan.ems -checks dp_pre_floorplan}
+}
+
+####################################
+# Floorplanning
+####################################
+initialize_floorplan -core_utilization 0.05
+save_lib -all
+
+####################################
+## PG Pin connections
+#####################################
+puts "RM-info : Running connect_pg_net -automatic on all blocks"
+connect_pg_net -automatic -all_blocks
+save_block -force       -label ${PRE_SHAPING_LABEL_NAME}
+save_lib -all
+
+####################################
+### Place IO
+######################################
+if {[file exists [which $TCL_PAD_CONSTRAINTS_FILE]]} {
+   puts "RM-info : Loading TCL_PAD_CONSTRAINTS_FILE file ($TCL_PAD_CONSTRAINTS_FILE)"
+   source -echo $TCL_PAD_CONSTRAINTS_FILE
+
+   puts "RM-info : running place_io"
+   place_io
+}
+
+save_block -hier -force   -label ${PLACE_IO_LABEL_NAME}
+save_lib -all
+
+####################################
+### Memory Placement
+######################################
+if [sizeof_collection [get_cells -hier -filter is_hard_macro==true -quiet]] {
+   set all_macros [get_cells -hier -filter is_hard_macro==true]
+   # Check top-level
+   report_macro_constraints -allowed_orientations -preferred_location -alignment_grid -align_pins_to_tracks $all_macros > $REPORTS_DIR_PLACEMENT/report_macro_constraints.rpt
+}
+
+###---Place Macro at user defined locations---### 
+#if {$USE_INCREMENTAL_DATA && [file exists $OUTPUTS_DIR/preferred_macro_locations.tcl]} {
+source $OUTPUTS_DIR/preferred_macro_locations.tcl
+#}
+
+####################################
+# Configure placement
+####################################
+if {$DISTRIBUTED} {
+   set HOST_OPTIONS "-host_options block_script"
+} else {
+   set HOST_OPTIONS ""
+}
+set CMD_OPTIONS "-floorplan $HOST_OPTIONS"
+
+########################################
+## Read parasitic files
+########################################
+if {[file exists [which $TCL_PARASITIC_SETUP_FILE]]} {
+   puts "RM-info : Sourcing [which $TCL_PARASITIC_SETUP_FILE]"
+   source -echo $TCL_PARASITIC_SETUP_FILE
+    } elseif {$TCL_PARASITIC_SETUP_FILE != ""} {
+   puts "RM-error : TCL_PARASITIC_SETUP_FILE($TCL_PARASITIC_SETUP_FILE) is invalid. Please correct it."
+    } else {
+   puts "RM-info : No TLU plus files sourced, Parastic library containing TLU+ must be included in library reference list"
+    }
+
+########################################
+## Read constraints
+########################################
+if {[file exists $TCL_MCMM_SETUP_FILE]} {
+   puts "RM-info : Loading TCL_MCMM_SETUP_FILE ($TCL_MCMM_SETUP_FILE)"
+   source -echo $TCL_MCMM_SETUP_FILE
+   } else {
+   puts "RM-error : Cannot find TCL_MCMM_SETUP_FILE ($TCL_MCMM_SETUP_FILE)"
+   error
+   }
+set CMD_OPTIONS "$CMD_OPTIONS -timing_driven"
+set plan.place.auto_generate_blockages true
+
+eval create_placement $CMD_OPTIONS
+report_placement    -physical_hierarchy_violations all    -wirelength all -hard_macro_overlap    -verbose high > $REPORTS_DIR_PLACEMENT/report_placement.rpt
+
+# write out macro preferred locations based on latest placement
+# If this file exists on subsequent runs it will be used to drive the macro placement
+if [sizeof_collection [get_cells -hier -filter is_hard_macro==true -quiet]] {
+   file delete -force $OUTPUTS_DIR/preferred_macro_locations.tcl
+   set all_macros [get_cells -hier -filter is_hard_macro==true]
+   derive_preferred_macro_locations $all_macros -file $OUTPUTS_DIR/preferred_macro_locations.tcl
+}
+
+####################################
+# Fix all shaped blocks and macros
+####################################
+if [sizeof_collection [get_cells -hier -filter is_hard_macro==true -quiet]] {
+   set_attribute -quiet [get_cells -hierarchical -filter is_hard_macro==true] status fixed
+}
+
+save_block -hier -force   -label ${PLACEMENT_LABEL_NAME}
+save_lib -all
+
+####################################
+# Create Power
+####################################
+if {[file exists $TCL_PNS_FILE]} {
+   puts "RM-info : Sourcing TCL_PNS_FILE ($TCL_PNS_FILE)"
+   source -echo $TCL_PNS_FILE
+}
+
+save_block -hier -force   -label ${CREATE_POWER_LABEL_NAME}
+save_lib -all
